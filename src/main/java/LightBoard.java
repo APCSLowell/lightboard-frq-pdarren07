7e283ba35fd119180b7bca2a323b7192bbd59697
pdarren07 <pdarren624@gmail.com>
2025-05-01 23:22:56 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,13 +11,13 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
     for(int i = 0; i < numRows; i++){
       for(int j = 0; j < numCols; j++){
         double random = Math.random();
         lights[i][j] = (random <= 0.4);
       }
     }
-    return lights;
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
